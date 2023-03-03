e73a01de0a55091883a0a505668c1cc9e6035e7b
limingchong <1013436135@qq.com>
2023-03-03 08:06:41 +0800

    small problem

--- emo_app/extracter.py
+++ emo_app/extracter.py
@@ -3,9 +3,6 @@ import nltk
 
 #nltk.download('stopwords')
 #nltk.download('punkt')
-text = """spaCy is an open-source software library for advanced natural language processing,
-written in the programming languages Python and Cython. The library is published under the MIT license
-and its main developers are Matthew Honnibal and Ines Montani, the founders of the software company Explosion."""
 
 
 def get_key_word(text):
